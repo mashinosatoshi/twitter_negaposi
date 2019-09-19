afec9f4d20c9c04d91d001b77c368137ae0b09b6
mashinosatoshi <mashino.satoshi@gmail.com>
2019-09-19 18:08:36 +0900

    文字カウント

--- execute.py
+++ execute.py
@@ -1,31 +1,43 @@
 # -*- coding: utf-8 -*-
-import json
+import json, collections
 import analyzeNP
-import getTimelineTweet
-import searchTweet
+from twitter_api import getTimelineTweet
+from twitter_api import searchTweet
 
 anp = analyzeNP.analyzeNP()
 
-"""
+
 req = getTimelineTweet.get_tweet('matsugae_tmyk', 200)
 
+"""
 if req.status_code == 200:
     res = json.loads(req.text)
     print(len(res))
-    sum_t = [anp.analyze(line['text']) for line in res]
-    print(sum_t)
-    print(sum(sum_t))
+    sum_n = 0
+    sum_c = collections.Counter()
+    for line in res:
+        num, counter = anp.analyze(line['text'])
+        sum_c.update(counter)
+        sum_n += num
+    print(sum_n)
+    print(sum_c)
 else:
     print("Failed: %d" % req.status_code)
 """
-
-req = searchTweet.get_tweet('ガンホー', 100)
+ward = 'パズドラ'
+req = searchTweet.get_tweet(ward, 100)
 
 if req.status_code == 200:
     res = json.loads(req.text)['statuses']
     print(len(res))
-    sum_t = [anp.analyze(line['text']) for line in res]
-    print(sum_t)
-    print(sum(sum_t))
+    sum_n = 0
+    sum_c = collections.Counter()
+    for line in res:
+        num, counter = anp.analyze(line['text'])
+        sum_c.update(counter)
+        sum_n += num
+    print(sum_n)
+    print(sum_c)
 else:
     print("Failed: %d" % req.status_code)
+#"""
