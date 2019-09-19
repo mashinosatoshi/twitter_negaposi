1200c49b326b965d73cbd080dcaabf9e3c9a7897
mashinosatoshi <mashino.satoshi@gmail.com>
2019-09-19 10:05:17 +0900

    検索昨日

--- execute.py
+++ execute.py
@@ -1,13 +1,28 @@
 # -*- coding: utf-8 -*-
 import json
-import getTimelineTweet, analyzeNP
+import analyzeNP
+import getTimelineTweet
+import searchTweet
 
 anp = analyzeNP.analyzeNP()
 
+"""
 req = getTimelineTweet.get_tweet('matsugae_tmyk', 200)
 
 if req.status_code == 200:
     res = json.loads(req.text)
+    print(len(res))
+    sum_t = [anp.analyze(line['text']) for line in res]
+    print(sum_t)
+    print(sum(sum_t))
+else:
+    print("Failed: %d" % req.status_code)
+"""
+
+req = searchTweet.get_tweet('ガンホー', 100)
+
+if req.status_code == 200:
+    res = json.loads(req.text)['statuses']
     print(len(res))
     sum_t = [anp.analyze(line['text']) for line in res]
     print(sum_t)
